f8f841dc6f7fa5d613ff7e5283408425ef8128e7
AnnaAndreeva92 <ann-stesha@yandex.ru>
2023-04-11 20:43:07 +0300

    Login test

--- src/test/java/ru/netology/page/DashboardPage.java
+++ src/test/java/ru/netology/page/DashboardPage.java
@@ -6,9 +6,9 @@ import com.codeborne.selenide.SelenideElement;
 import static com.codeborne.selenide.Selenide.$;
 
 public class DashboardPage {
-    private SelenideElement dashboard = $("[data-test-id=dashboard]");
+    private SelenideElement dashBoardHeading = $("h2[data-test-id='dashboard']");
 
-    public void dashboardPageVisible() {
-        dashboard.shouldBe(Condition.visible);
+    public void checkIfVisible() {
+        dashBoardHeading.shouldBe(Condition.visible);
     }
 }
--- src/test/java/ru/netology/page/VerificationPage.java
+++ src/test/java/ru/netology/page/VerificationPage.java
@@ -1,20 +1,14 @@
 package ru.netology.page;
 
-import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
-
 import static com.codeborne.selenide.Selenide.$;
 
 public class VerificationPage {
-    private SelenideElement codeField = $("[data-test-id=code] input");
+    private SelenideElement verificationField = $("input[name=\"code\"]");
     private SelenideElement verifyButton = $("[data-test-id=action-verify]");
 
-    public VerificationPage() {
-        codeField.shouldBe(Condition.visible);
-    }
-
-    public DashboardPage validVerify(String code) {
-        codeField.setValue(code);
+    public DashboardPage verify(String code) {
+        verificationField.setValue(code);
         verifyButton.click();
         return new DashboardPage();
     }
